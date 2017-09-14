fd76b28e85b055d446e9c3541dbdb673573b60c9
yp-tc-m-2505 <yp-tc-m-2505@yp-tc-m-2505deMacBook-Air.local>
2017-09-14 18:01:19 +0800

    test shell

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 git add .
-git commit -m "change something"
+# git commit -m "change something"
+git commit -m "$1"
 git push
---
+++ tt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [$1 =="tart"]
+then
+    echo "do start"
+elif [$s1 =="stop"]
+then
+    echo "do stop"
+else
+    echo "please nput start or stop"
+fi
